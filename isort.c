9820ddf03665b91a4bed2221c0ba7afc6f0845f0
slash <sahmedk2@outlook.com>
2016-12-13 19:09:09 +0400

    Doesn't show output.

--- isort.c
+++ isort.c
@@ -8,5 +8,12 @@
 
 int main(int argc, char* argv[])
 {
-
+    //printf("In main.\n");
+    int array_len;
+    if (!argc)
+    {
+        printf("\nNo inputer parameters detected. Enter array length:\n");
+        while (!(scanf("length: %d", &array_len)==1));
+    }
+    array_len = argc;
 }
